21d01b0ad69afe690c6e5249ec4264aaeba9452c
Yakup Sadi <sadiyakupsadi@hotmail.com>
2023-03-05 02:42:23 +0300

    5 Mar 2023

--- server/routers/folder.js
+++ server/routers/folder.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const Folder = express.Router()
+const auth = require('../middleware/auth')
 
 const { 
     getFolder,
@@ -11,10 +12,10 @@ const {
 } = require('../controller/folder')
 
 Folder.route('/folder').get(getAllFolder)
-Folder.route('/folder/createFolder').post(createFolder)
+Folder.route('/folder/createFolder').post(auth, createFolder)
 
 Folder.route('/folder/:id')
-    .put(updateFolder)
+    .put(auth, updateFolder)
     .delete(deleteFolder)
 
 module.exports = Folder
